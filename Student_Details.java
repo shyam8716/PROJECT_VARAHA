e73339cf48c393c4c9da01b8fca20de51739cf7f
R. megha shyam <meghashyam997@gmail.com>
2026-03-16 20:08:11 +0530

    UPDATED JAVA FILES IN BRANCH1

--- Student_Details.java
+++ Student_Details.java
@@ -1,8 +1,12 @@
 class Student_Details{
-    static String name="R.megha shyam";
-     static int roll_number=3;
+    static String name1="R.megha shyam";
+     static int roll_number1=3;
+     static String name2="girish";
+     static int roll_number2=6;
     public static void main(String[] args) {
-        System.out.println("my name is: "+name);
-        System.out.println("my roll number is"+roll_number);
+        System.out.println("my name is: "+name1);
+        System.out.println("my roll number is"+roll_number1);
+        System.out.println("my name is: "+name2);
+        System.out.println("my roll number is"+roll_number2);
     }
 }
--- Subject_Marks.java
+++ Subject_Marks.java
@@ -4,8 +4,10 @@
     static int m1=5;
     static int m2=10;
     static int m3=15;
-    static int total=m1+m2+m3;
-    static int average=total/3;
+    static int m4=20;
+    static int m5=25;
+    static int total=m1+m2+m3+m4+m5;
+    static int average=total/5;
     public static void main(String[] args) {
         System.out.println("my name is "+name);
         System.out.println("my roll number is"+roll_number);
