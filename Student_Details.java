2dd49c3ba9de6067f2466b5117eacbb51abbec6f
R. megha shyam <meghashyam997@gmail.com>
2026-03-16 19:29:20 +0530

    ALL JAVA FILES ARE COMMITED IN MASTER BRANCH

---
+++ Student_Details.java
@@ -0,0 +1,8 @@
+class Student_Details{
+    static String name="R.megha shyam";
+     static int roll_number=3;
+    public static void main(String[] args) {
+        System.out.println("my name is: "+name);
+        System.out.println("my roll number is"+roll_number);
+    }
+}
---
+++ Subject_Marks.java
@@ -0,0 +1,15 @@
+ class Subject_Marks {
+    static String name="girish";
+    static int roll_number=3;
+    static int m1=5;
+    static int m2=10;
+    static int m3=15;
+    static int total=m1+m2+m3;
+    static int average=total/3;
+    public static void main(String[] args) {
+        System.out.println("my name is "+name);
+        System.out.println("my roll number is"+roll_number);
+        System.out.println("my total marks are "+total);
+        System.out.println("my average marks are "+average);
+    }
+}
